bb3cd1f1a3be5c71cdbd6299a5d4523aba109116
YorovSobir <sobir.yorov94@gmail.com>
2017-12-24 15:33:03 +0300

    remove first task from branch

--- juliaSet/src/util.h
+++
@@ -1,9 +0,0 @@
-#ifndef UTIL_H
-#define UTIL_H
-
-#include <GL/glew.h>
-
-GLuint loadShaders(const char* vertexFilePath, const char* fragmentFilePath);
-void loadTexture(const char * fname);
-
-#endif // UTIL_H
